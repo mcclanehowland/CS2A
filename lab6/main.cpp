2c0694e281f489c577fd970f01f7c0fc46e3685b
McClane Howland <mcclane.howland@gmail.com>
2016-12-07 20:24:56 -0800

    finished everything but main class, need info on pointers

--- lab6/main.cpp
+++ lab6/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <ctime>
 
 using namespace std;
 
@@ -64,7 +65,7 @@ void Fruit::Display() {
 
 class OnlineSuperMarket {
     private: 
-        Fruit fruits[];
+        Fruit fruits[8];
         string market_name_;
         string web_address_;
         static double tax_rate;
@@ -82,7 +83,7 @@ class OnlineSuperMarket {
         void Init();
         void Sort();
         void Run();
-        void Find(string name);
+        int Find(string name);
         void Quit();
         void ShowAll();
         static int get_tax_rate();
@@ -109,12 +110,78 @@ void OnlineSuperMarket::set_name(string name) {
 }
 //member functions
 void OnlineSuperMarket::Init() {
-    //for(int i = 0;i < 
+
+    string names[8] = {"Banana", "Strawberry", "Orange", "Grapes", "Watermelon", "Apple", "Blueberry", "Mango"};
+    float LO[8] = {0.5,2.1,3,2.15,0.2,1.5,3,0.75};
+    float HI[8] = {0.85,3.5,3.5,4.5,0.45,3.2,4.5,1.25};
+
+    srand(static_cast<float>(time(0)));
+
+    for(int i = 0;i < 8;i++) {
+        fruits[i].set_name(names[i]);
+        fruits[i].set_price(LO[i]+rand()%(HI-LO));
+        fruits[i].set_weight(10+rand()%40);
+    }
     
 }
-
-
+void OnlineSuperMarket::Sort() {
+    for(int i = 0;i < 7;i++) {
+        for(int j = i+1;j < 8;j++) {
+            if(fruits[j].get_name() < fruits[i].get_name()) {
+                Fruit temp = fruits[j];
+                fruits[j] = fruits[i];
+                fruits[i] = temp;
+            }
+        } 
+    }
+}
+void OnlineSuperMarket::Quit() {
+    cout<<"Thank you for your visit and come again\n";
+    exit(0);
+}
+int OnlineSuperMarket::Find(string name) {
+    for(int i = 0;i < 8;i++) {
+        if(fruits[i].get_name() == name) {
+            return i;
+        }
+    }
+    return -1;
+}
+void OnlineSuperMarket::Run() {
+    cout<<market_name_<<endl<<web_address_<<endl;
+    while(true) {
+        cout<<"Enter a fruit name or XXX to end: ";
+        string name;
+        cin >>name;
+        if(name == "XXX") {
+            Quit();
+        }
+        int index = Find(name);
+        if(index == -1) {
+            cout<<"invalid fruit name";
+            continue;
+        }
+        cout<<"\nEnter weight in lbs: ";
+        float weight;
+        cin >>weight;
+        float price = fruits[index].Order(weight);
+        if(price == -1) {
+            cout<<"weight too large\n";
+            continue;
+        }
+        float total_price = price + price*tax_rate;
+        cout<<"Total cost: $"<<setprecision(2)<<total_price<<endl;
+    }
+}
+void OnlineSuperMarket::ShowAll() {
+    for(int i = 0;i < 8;i++) {
+        fruits[i].Display();
+    }
+}
 
 int main() {
+    OnlineSuperMarket* super = NULL;
+    OnlineSuperMarket *market = new OnlineSuperMarket("Foothill Supermarket","www.supermarket.com");
+    super = market;
     return 0;
 }
